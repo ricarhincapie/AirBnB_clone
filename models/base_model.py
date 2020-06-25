c8bd0798b0f3774c61fa44397a1b05cace0a3cce
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-06-25 22:00:57 +0000

    Task #1 done

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,18 @@ import uuid
 import datetime
 
 class BaseModel():
-	def __init__(self):
-		self.id = str(uuid.uuid4())
-		self.updated_at = datetime.datetime.now()
-		self.created_at = datetime.datetime.now()
-	
+	def __init__(self, *args, **kwargs):
+		if not kwargs:
+			self.id = str(uuid.uuid4())
+			self.updated_at = datetime.datetime.now()
+			self.created_at = datetime.datetime.now()
+		else:
+			for charac in kwargs:
+				if charac == "__class__":
+					continue
+				setattr(self, charac, kwargs[charac])
+			self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+
 	def __str__(self):
 		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
@@ -21,5 +28,3 @@ class BaseModel():
 		dic["created_at"] = dic["created_at"].isoformat()
 		dic["updated_at"] = dic["updated_at"].isoformat()
 		return dic
-
-print(BaseModel().to_dict())
