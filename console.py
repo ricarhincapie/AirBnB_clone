8507c4b9c9b7951b36a4baff29e6dfbdb08a853f
kajoomba99 <steven.brand1999gmail.com>
2020-06-26 19:02:50 -0500

    console.py show create and destroy method added

--- console.py
+++ console.py
@@ -2,20 +2,71 @@
 
 import sys
 import cmd
+from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb) "
+	prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        """Quit command to exit the program
-        """        
-        sys.exit()
-    
-    do_EOF = do_quit
+	def do_quit(self, arg):
+		"""Quit command to exit the program
+		"""        
+		sys.exit()
 
-    def emptyline(self):
-        pass
-    
+	def do_EOF(self, arg):
+		"""EOF command to exit the program
+		"""
+		sys.exit()
+
+	def emptyline(self):
+		pass
+
+	def do_create(self, arg):
+		if len(arg) <= 0:
+			print("** class name missing **")
+		else:
+			if arg == "BaseModel":
+				new_obj = BaseModel()
+				models.storage.save()
+				print(new_obj.id)
+			else:
+				print("** class doesn't exist **")
+	
+	def do_show(self, arg):
+		if len(arg) <= 0:
+			print("** class name missing **")
+		else:
+			arguments = arg.split()
+			if len(arguments) == 1:
+				print("** instance id missing **")
+			else:
+				if arguments[0] != "BaseModel":
+					print("** class doesn't exist **")
+				else:
+					objects = models.storage.all()
+					try:
+						print(objects[".".join(arguments)])
+					except:
+						print("** no instance found **")
+
+	def do_destroy(self, arg):
+		if len(arg) <= 0:
+			print("** class name missing **")
+		else:
+			arguments = arg.split()
+			if arguments[0] != "BaseModel":
+				print("** class doesn't exist **")
+			else:
+				if len(arguments) == 1:
+					print("** instance id missing **")
+				else:
+					objects = models.storage.all()
+					try:
+						del objects[".".join(arguments)]
+						models.storage.save()
+						models.storage.reload()
+					except:
+						print("** no instance found **")
 
 if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+	HBNBCommand().cmdloop()
